92bb677372dd594376fa3a6453a05dcce808c2c8
Gabriele De Luca <35374403+gabrieledeluca@users.noreply.github.com>
2019-01-31 15:42:15 +0100

    Update add-test.js

--- test/unit/add-test.js
+++ test/unit/add-test.js
@@ -235,12 +235,10 @@ describe('Verify Northbound flow', function() {
 
     it('verify commands execution as context provider', function(done) {
       this.timeout(0);
-<<<<<<< HEAD
-      console.log('verify commands execution as context provider');
-=======
-           console.log("TEST verify commands execution as context provider");
 
->>>>>>> 257dbcbb07ea8eb28955d2ccb4382ee612673622
+      console.log("TEST verify commands execution as context provider");
+
+
       // Run test
       //STOP CAR
       var json={};
