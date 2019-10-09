a40b9b6414937ed0fa62742921367245115cd2ee
PietroGreco <pietro.greco@eng.it>
2019-10-09 09:44:44 +0200

    Cleaning up

--- TESTS/TO_INTEGRATE/test.js
+++
@@ -1,8 +0,0 @@
-const verifyAdminAPI = require('./verifyAdminAPI');
-
-describe('verify admin api', function() {
-    it('verify version service', verifyAdminAPI.verifyVersionService);
-    it('verify status service', verifyAdminAPI.verifyStatusService);
-    it('verify config service', verifyAdminAPI.verifyConfigService);
-    it('verify commandsList service', verifyAdminAPI.verifyCommandsListService);
-});
