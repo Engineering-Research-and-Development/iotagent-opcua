50b107fb8a44d23ea7197b8afe8cbe45d324d246
trimart <57678398+trimart@users.noreply.github.com>
2020-10-07 13:30:26 +0200

    Update travis_ip.sh

--- travis_ip.sh
+++ travis_ip.sh
@@ -4,3 +4,4 @@ MACHINE_IP=$(hostname -I | cut -d ' ' -f 1)
 
 sed -i "s/TEST_MACHINE_IP/$MACHINE_IP/g" conf/config.properties
 sed -i "s/TEST_MACHINE_IP/$MACHINE_IP/g" conf/config.json
+sed -i "s/TEST_MACHINE_IP/$MACHINE_IP/g" conf/config.json
