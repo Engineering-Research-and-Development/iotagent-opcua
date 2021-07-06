5b47d6ef674d74040bc1b6571f886a0ba4f7efab
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-07-06 09:14:28 +0200

    fix load test conf

--- tests/load_test_configuration.sh
+++ tests/load_test_configuration.sh
@@ -2,5 +2,3 @@
 
 cp conf/config.json.TEST_WITH_PLACEHOLDER conf/config.json
 cp conf/config.properties.WITH_PLACEHOLDER conf/config.properties
-cp conf/config.json.TEST_WITH_PLACEHOLDER conf/config.json.TEST_WITH_PLACEHOLDER
-cp conf/config.properties.WITH_PLACEHOLDER conf/config.properties.WITH_PLACEHOLDER
