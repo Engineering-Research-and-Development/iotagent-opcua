68bc96899ff90332d2a9466f5c19d85aef507757
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2022-08-02 11:16:46 +0200

    fix lazy attr

--- iot_agent_modules/run/createResponse.js
+++ iot_agent_modules/run/createResponse.js
@@ -1,21 +1,29 @@
 module.exports = {
     createResponse: function(id, type, attributes, body, metadata) {
         var values = body.split(',');
-        var responses = [];
+        //var responses = [];
+
+        var returnObj = {
+            id: id,
+            type: type
+        };
 
         for (var i = 0; i < attributes.length; i++) {
-            responses.push({
+            var obj = {};
+            /* responses.push({
                 name: attributes[i],
                 type: 'string',
                 value: values[i],
                 metadatas: metadata[i]
-            });
+            });*/
+            obj[attributes[i]] = {
+                type: 'string',
+                value: values[i]
+            };
+            Object.assign(returnObj, obj);
         }
+        console.log(returnObj);
 
-        return {
-            id: id,
-            type: type,
-            attributes: responses
-        };
+        return returnObj;
     }
 };
