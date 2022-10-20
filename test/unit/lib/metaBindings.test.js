1c88802a7a7eadc6087bfe66c98c3bf6f00b9295
ManfriPist <30775241+manfredipist@users.noreply.github.com>
2022-10-20 18:08:45 +0200

    fix metaBindings bug and test for metaBindings

--- test/unit/lib/metaBindings.test.js
+++ test/unit/lib/metaBindings.test.js
@@ -1,20 +1,115 @@
 const rewire = require('rewire');
-const chai = require('chai');
+const mockConfig = require('../../../conf/config-v2.example');
 
 describe('MetaBinding handling', () => {
     const metaBindings = rewire('../../../lib/metaBindings.js');
-    const mockConfig = rewire('../../../conf/config-v2.example');
-
-    metaBindings.__set__('httpRequest', (options, body) => {
-        return new Promise(function (resolve, reject) {
-            resolve(true);
-        });
-    });
 
     describe('Make auto-provisioning of Group and Device', () => {
         beforeEach(() => {
             metaBindings.__set__('config.getConfig', () => {
                 return mockConfig;
+            });
+            metaBindings.__set__('http.request', (options, res_callback) => {
+                return {
+                    on: function(type, on_callback){},
+                    write: function(string){},
+                    end: function(){
+                        res_callback({
+                            statusCode: 204,
+                            on: function(type, chunk_callback){
+                                if(type==="data"){
+                                    chunk_callback(new Buffer.from("{}"));
+                                }
+                                if(type==="end"){
+                                    chunk_callback();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        });
+
+        describe('When createGroup and createDevicesResult are true', () => {
+            it('Should make autoprovisioning', (done) => {
+                metaBindings
+                    .performAutoProvisioning()
+                    .then(function () {
+                        done();
+                    })
+                    .catch(function () {
+                        done();
+                    });
+            });
+        });
+
+        describe('When createGroup and createDevicesResult are true and device already exists', () => {
+            beforeEach(() => {
+                metaBindings.__set__('http.request', (options, res_callback) => {
+                    return {
+                        on: function(type, on_callback){},
+                        write: function(string){},
+                        end: function(){
+                            res_callback({
+                                statusCode: 409,
+                                on: function(type, chunk_callback){
+                                    if(type==="data"){
+                                        chunk_callback(new Buffer.from("{}"));
+                                    }
+                                    if(type==="end"){
+                                        chunk_callback();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            });
+
+            it('Should make autoprovisioning', (done) => {
+                metaBindings
+                    .performAutoProvisioning()
+                    .then(function () {
+                        done();
+                    })
+                    .catch(function () {
+                        done();
+                    });
+            });
+        });
+
+        describe('When createGroup and createDevicesResult are true and error is returned', () => {
+            beforeEach(() => {
+                metaBindings.__set__('http.request', (options, res_callback) => {
+                    return {
+                        on: function(type, on_callback){},
+                        write: function(string){},
+                        end: function(){
+                            res_callback({
+                                statusCode: 400,
+                                on: function(type, chunk_callback){
+                                    if(type==="data"){
+                                        chunk_callback(new Buffer.from("{}"));
+                                    }
+                                    if(type==="end"){
+                                        chunk_callback();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            });
+
+            it('Should make autoprovisioning', (done) => {
+                metaBindings
+                    .performAutoProvisioning()
+                    .then(function () {
+                        done();
+                    })
+                    .catch(function () {
+                        done();
+                    });
             });
         });
 
@@ -27,10 +122,13 @@ describe('MetaBinding handling', () => {
                     return true;
                 });
             });
+
             it('Should throw a new Error exception', (done) => {
                 metaBindings
                     .performAutoProvisioning()
-                    .then(function () {})
+                    .then(function () {
+                        done();
+                    })
                     .catch(function () {
                         done();
                     });
@@ -50,31 +148,14 @@ describe('MetaBinding handling', () => {
             it('Should throw a new Error exception', (done) => {
                 metaBindings
                     .performAutoProvisioning()
-                    .then(function () {})
+                    .then(function () {
+                        done();
+                    })
                     .catch(function () {
                         done();
                     });
             });
         });
 
-        describe('When createGroup and createDevicesResult are true', () => {
-            beforeEach(() => {
-                metaBindings.__set__('createGroup', () => {
-                    return false;
-                });
-                metaBindings.__set__('createDevices', () => {
-                    return false;
-                });
-            });
-
-            it('Should throw a new Error exception', (done) => {
-                metaBindings
-                    .performAutoProvisioning()
-                    .then(function () {})
-                    .catch(function () {
-                        done();
-                    });
-            });
-        });
     });
 });
